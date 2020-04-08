d80ccf2963bbc7b58d93303862f159c0489ba30a
nathanwang <wang.p@husky.neu.edu>
2020-04-08 00:43:38 -0400

    created user list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useEffect} from 'react';
+import {Grid, Paper,Checkbox} from '@material-ui/core';
+import {TableContainer,Table,TableBody,TableCell,TableHead,TableRow} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const classes = useStyles();
+
+  function getUserList() {
+    axios.get("http://localhost:8080/api/user/description")
+    .then(function(response) {
+      console.log(response);
+      setUserList([].concat(response['data']))
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    getUserList();
+  }, []);
+  
+  const colums=[
+      {key: 'column1', name:'UserName', fileName: 'userName', minWidth: 100, maxWidth: 200, isResizable: true},
+      {key: 'column2', name:'Password', fileName: 'passWord', minWidth: 100, maxWidth: 200, isResizable: true},
+    ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container justify="center">
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>UserName</TableCell>
+                  <TableCell>Password</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                  {userList.map((row) => (
+                    <TableRow key={row['userName']}>
+                      <TableCell component="th" scope="row">{row['userName']}</TableCell>
+                      <TableCell>{row['password']}</TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
     </div>
+      
   );
 }
 
